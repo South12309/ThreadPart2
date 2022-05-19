791c6356d487f109c73bf7269fd97989b4428545
South12309 <south123@mail.ru>
2022-05-19 14:18:37 +0000

    Add together start race and count together throw tunnel

--- src/com/gb/java/Car.java
+++ src/com/gb/java/Car.java
@@ -1,5 +1,7 @@
 package com.gb.java;
 
+import java.util.concurrent.CyclicBarrier;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
 
@@ -10,6 +12,7 @@ public class Car implements Runnable {
     private Race race;
     private int speed;
     private String name;
+    private CyclicBarrier clb;
 
     public String getName() {
         return name;
@@ -19,11 +22,12 @@ public class Car implements Runnable {
         return speed;
     }
 
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, CyclicBarrier clb) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.clb = clb;
     }
 
     @Override
@@ -32,6 +36,7 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int) (Math.random() * 800));
             System.out.println(this.name + " готов");
+            clb.await();
         } catch (Exception e) {
             e.printStackTrace();
         }
